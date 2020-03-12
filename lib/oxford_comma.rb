df12cca6d758d58f09fafe7f6fe22274c01e1bf6
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-12 02:15:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(phrase)
+  if phrase.length == 1 
+    return phrase[0]
+  elsif phrase.length == 2 
+    return "#{phrase[0]} and #{phrase[1]}"
+  else 
+    
+  end  
+end     
+
