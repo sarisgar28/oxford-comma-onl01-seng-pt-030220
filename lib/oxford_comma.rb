b15c815e98c6e9343bcd195534f16988ff620adc
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-12 02:22:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
-
+require "pry"
 def oxford_comma(phrase)
   if phrase.length == 1 
     return phrase[0]
   elsif phrase.length == 2 
     return "#{phrase[0]} and #{phrase[1]}"
   else 
+    binding.pry 
     return phrase.slice(0,-2)
   end  
 end     
